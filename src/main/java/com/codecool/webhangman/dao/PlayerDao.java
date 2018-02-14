ec92c002767fd844067791acc238621223b3fd24
monzag <zaganiacz.m@gmail.com>
2018-02-14 12:03:06 +0100

    Add Repository annotation

--- src/main/java/com/codecool/webhangman/dao/PlayerDao.java
+++ src/main/java/com/codecool/webhangman/dao/PlayerDao.java
@@ -2,6 +2,8 @@ package com.codecool.webhangman.dao;
 
 import com.codecool.webhangman.model.Player;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlayerDao extends CrudRepository<Player, Integer> {
 }
