03df896056a0302b41bd1796edcb193ac985877a
monzag <zaganiacz.m@gmail.com>
2018-02-14 10:53:59 +0100

    Add WebHangmanApplication

---
+++ src/main/java/com/codecool/webhangman/WebHangmanApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.webhangman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHangmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebHangmanApplication.class, args);
+	}
+}
