da0bb19959d1a30e3583fe726907082f4ff2cd26
monzag <zaganiacz.m@gmail.com>
2018-02-14 12:22:20 +0100

    Add getStartScreen method

--- src/main/java/com/codecool/webhangman/controller/HangmanHandler.java
+++ src/main/java/com/codecool/webhangman/controller/HangmanHandler.java
@@ -1,14 +1,22 @@
 package com.codecool.webhangman.controller;
 
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/hangman")
+@RestController
+@RequestMapping("/")
 public class HangmanHandler {
 
 
-    @GetMapping
-    public void doGet() {
+    @GetMapping(path = "")
+    public String getStartScreen() {
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("/templates/startScreen.twig");
+        JtwigModel model = JtwigModel.newModel();
 
+        return template.render(model);
     }
 }
