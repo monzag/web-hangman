c319aca5bdb73303af28940e5556f90b86be65be
monzag <zaganiacz.m@gmail.com>
2018-02-14 12:02:53 +0100

    Add Repository annotation

--- src/main/java/com/codecool/webhangman/dao/CountryDao.java
+++ src/main/java/com/codecool/webhangman/dao/CountryDao.java
@@ -2,5 +2,7 @@ package com.codecool.webhangman.dao;
 
 import com.codecool.webhangman.model.Country;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CountryDao extends CrudRepository<Country, Integer> {}
