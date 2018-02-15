5ccf2030e857c6c41997780e69c9855e9dba5080
Kamil <blueflamesxcode@gmail.com>
2018-02-15 19:21:13 +0100

    add part of post handling in game ctrl

--- src/main/java/com/codecool/webhangman/controller/GameHandler.java
+++ src/main/java/com/codecool/webhangman/controller/GameHandler.java
@@ -1,21 +1,33 @@
 package com.codecool.webhangman.controller;
 
+import com.codecool.webhangman.model.GuessTable;
+import com.codecool.webhangman.model.Player;
+import com.codecool.webhangman.model.TemplateProcessorFacade;
+import com.codecool.webhangman.service.GameBoardService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/hangman")
 public class GameHandler {
+    private GameBoardService gameBoardService;
+
+    public GameHandler(GameBoardService gameBoardService) {
+        this.gameBoardService = gameBoardService;
+    }
 
     @GetMapping
     public String doGet(HttpServletResponse response, HttpServletRequest request) {
         TemplateProcessorFacade processor = new TemplateProcessorFacade("/templates/startScreen.twig");
+
+
+        String contentCss = "classpath:/" + "templates/cssSettings/game-css-snippet.html";
+        processor.modelWith("content_css", contentCss);
 
         String contentPath = "classpath:/" + "templates/backgroundsnippets/game-snippet.html";
         processor.modelWith("content_path", contentPath);
@@ -23,16 +35,15 @@ public class GameHandler {
         return processor.render();
     }
 
+    @PostMapping
+    public String doPost(HttpServletRequest request) {
+        SessionInterpreter.RequestInterpreter requestInterpreter = SessionInterpreter.create(request);
 
-    public String doPost(HttpServletResponse response, HttpServletRequest request) {
+        Player player = requestInterpreter.retrievePlayer();
+        GuessTable guessTable = requestInterpreter.retrieveGuessTable();
+        String userGuess = request.getParameter("user-guess");
+        this.gameBoardService.handleGame(guessTable, player, userGuess);
 
-//
-//
-//        String contentPath = "classpath:/" + managerOption.getPath();
-//        model.with("content_path", contentPath);
-
-
-
-        return null;
+        return "LOSEE HP";
     }
 }
