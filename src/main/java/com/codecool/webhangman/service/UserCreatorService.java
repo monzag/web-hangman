124711dbb1a7956cf55a875bc11c11d1d58b1138
Kamil <blueflamesxcode@gmail.com>
2018-02-15 19:15:15 +0100

    Add UserCreatorService

---
+++ src/main/java/com/codecool/webhangman/service/UserCreatorService.java
@@ -0,0 +1,24 @@
+package com.codecool.webhangman.service;
+
+import com.codecool.webhangman.enums.Identity;
+import com.codecool.webhangman.model.Player;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Service
+public class UserCreatorService {
+
+    public Player createPlayer(HttpServletRequest request) {
+        String nick = request.getParameter("nick");
+        return new Player(nick);
+    }
+
+    public void registerUserInSession(Player player, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+
+        session.setAttribute(Identity.PLAYER.getKey(), player);
+        session.setAttribute(Identity.IS_LOGGED_IN.getKey(), true);
+    }
+}
