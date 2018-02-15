5f40948c44bc5a2c97544981c022aa38b054d687
Kamil <blueflamesxcode@gmail.com>
2018-02-15 10:47:26 +0100

    Add config for handlerinterceptor

---
+++ src/main/java/com/codecool/webhangman/controller/WebConfig.java
@@ -0,0 +1,27 @@
+package com.codecool.webhangman.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    SessionManager getSessionManager() {
+        return new SessionManager();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(getSessionManager())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/resources/**");
+        // assuming you put your serve your static files with /resources/ mapping
+        // and the pre login page is served with /login mapping
+    }
+
+}
