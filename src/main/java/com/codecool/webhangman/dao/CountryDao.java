549704c83e79ec60c82cee3ac3e5df732ed76ffe
Kamil <blueflamesxcode@gmail.com>
2018-02-15 19:14:05 +0100

    Add query for retrieving random country

--- src/main/java/com/codecool/webhangman/dao/CountryDao.java
+++ src/main/java/com/codecool/webhangman/dao/CountryDao.java
@@ -1,8 +1,14 @@
 package com.codecool.webhangman.dao;
 
 import com.codecool.webhangman.model.Country;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CountryDao extends CrudRepository<Country, Integer> {}
+public interface CountryDao extends CrudRepository<Country, Integer> {
+    @Query(value="SELECT * FROM country ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+    Country getRandomCountry();
+}
