1607693f188b77bd7d3bf7da83143dcc1f81e5d9
monzag <zaganiacz.m@gmail.com>
2018-02-16 16:55:03 +0100

    Add isUserWinOrLose to preHandle

--- src/main/java/com/codecool/webhangman/controller/SessionManager.java
+++ src/main/java/com/codecool/webhangman/controller/SessionManager.java
@@ -19,9 +19,10 @@ public class SessionManager implements HandlerInterceptor{
         String currentPath = request.getServletPath();
 
         Function<String, Boolean> isUserAtGameUrl = p -> p.equals("/hangman");
+        Function<String, Boolean> isUserWinOrLose = p -> p.equals("/hangman/win");
         Function<String, Boolean> isUserAtLoginUrl = p -> p.equals("/");
 
-        if (isUserLoggedIn && !isUserAtGameUrl.apply(currentPath)) {
+        if (isUserLoggedIn && (!isUserAtGameUrl.apply(currentPath) && !isUserWinOrLose.apply(currentPath))) {
             response.sendRedirect("/hangman");
             return false;
 
