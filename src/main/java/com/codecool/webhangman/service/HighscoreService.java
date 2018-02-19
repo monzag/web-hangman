a93e0fd6f7afb04be6812c7347ab1953bfb7091b
monzag <zaganiacz.m@gmail.com>
2018-02-19 19:43:39 +0100

    Create HighscoreService

---
+++ src/main/java/com/codecool/webhangman/service/HighscoreService.java
@@ -0,0 +1,16 @@
+package com.codecool.webhangman.service;
+
+import com.codecool.webhangman.dao.ScoreDao;
+import com.codecool.webhangman.model.Player;
+import com.codecool.webhangman.model.Score;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HighscoreService {
+
+    private ScoreDao scoreDao;
+
+
+}
