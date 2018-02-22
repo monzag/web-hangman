2bd8adb069ae57ba18abcb791d44a05ca4a8a041
Kamil <blueflamesxcode@gmail.com>
2018-02-22 12:43:28 +0100

    Add put available routes into enums

---
+++ src/main/java/com/codecool/webhangman/enums/LoggedInDirectoriesContainer.java
@@ -0,0 +1,25 @@
+package com.codecool.webhangman.enums;
+
+import com.codecool.webhangman.service.permissionsmanagementservice.DefaultRoute;
+import com.codecool.webhangman.service.permissionsmanagementservice.UrlPaths;
+import com.codecool.webhangman.service.permissionsmanagementservice.WebDirectory;
+
+@UrlPaths
+public enum LoggedInDirectoriesContainer implements WebDirectory {
+    @DefaultRoute
+    MAIN_HANGMAN_VIEW("/hangman"),
+    GAME_END("/hangman/end"),
+    GAME_EXIT("/hangman/exit"),
+    GAME_RULES("/hangman/rules");
+
+    private String path;
+
+    LoggedInDirectoriesContainer(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public String getPath( ) {
+        return path;
+    }
+}
