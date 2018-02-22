e91fb53131f968bdb7529caddbdeea01dda457dc
Kamil <blueflamesxcode@gmail.com>
2018-02-22 14:04:00 +0100

    Create class which handle session progress analysis

--- src/main/java/com/codecool/webhangman/service/GameStateAnalyserService.java
+++ src/main/java/com/codecool/webhangman/service/GameStateAnalyserService.java
@@ -7,33 +7,11 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-public class GameBoardService {
-
+public class GameStateAnalyserService {
     private DrawService drawService;
 
-    public GameBoardService() {
-        this.drawService = new DrawService();
-    }
-
-    public void doNextMove(GuessTable guessTable, Player player, String userGuess) {
-        if (guessTable.isAlreadyUsed(userGuess)) {
-            return;
-        }
-
-        if (!guessTable.doCountryNameContains(userGuess)) {
-            reducePlayerHealthBasedOnGuess(userGuess, player);
-        }
-
-        guessTable.collectSentenceToProperContainer(userGuess);
-    }
-
-    private void reducePlayerHealthBasedOnGuess(String guess, Player player) {
-        if (guess.length() == 1) {
-            player.reduceHealthPoints(1);
-
-        } else if (guess.length() > 1) {
-            player.reduceHealthPoints(2);
-        }
+    public GameStateAnalyserService(DrawService drawService) {
+        this.drawService = drawService;
     }
 
     public String getCapitalAsGuess(GuessTable guessTable) {
