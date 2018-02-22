077d72e940d33aef218a8bce28a65500985fade4
Kamil <blueflamesxcode@gmail.com>
2018-02-22 14:05:21 +0100

    Reduce class responsibilities

---
+++ src/main/java/com/codecool/webhangman/service/GameLeadService.java
@@ -0,0 +1,32 @@
+package com.codecool.webhangman.service;
+
+import com.codecool.webhangman.model.GuessTable;
+import com.codecool.webhangman.model.Player;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class GameLeadService {
+
+    public void doNextMove(GuessTable guessTable, Player player, String userGuess) {
+        if (guessTable.isAlreadyUsed(userGuess)) {
+            return;
+        }
+
+        if (!guessTable.doCountryNameContains(userGuess)) {
+            reducePlayerHealthBasedOnGuess(userGuess, player);
+        }
+
+        guessTable.collectSentenceToProperContainer(userGuess);
+    }
+
+    private void reducePlayerHealthBasedOnGuess(String guess, Player player) {
+        if (guess.length() == 1) {
+            player.reduceHealthPoints(1);
+
+        } else if (guess.length() > 1) {
+            player.reduceHealthPoints(2);
+        }
+    }
+}
